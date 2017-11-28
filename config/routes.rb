bf8f8c006aca07d304d538cb5855f86624748f48
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-11-28 17:01:14 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+resources :authors, only: [:show, :new, :create]
+resources :posts, only: [:show, :edit, :update]
 end
